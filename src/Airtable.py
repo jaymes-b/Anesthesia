262b41d636fb9023ecdb5ab312a0d1514ce5b9f0
Aznpepsi <Jamesbanh@yahoo.com>
2022-10-14 17:00:26 -0700

    get data from airtable

---
+++ src/Airtable.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+class Airtable:
+    def __init__(self):
+        api_key = "put api key here"
+        self.headers = {"Authorization": "Bearer " + api_key}
+        self.table_names = ["body-part", "block", "surgery", "references", "surgeon-preference", "keywords", "surgeon"]
+        self.base_id = "shrPk12YITH9VeP7j"
+        self.url = "https://api.airtable.com/v0/" + self.base_id
+
+    def getData(self): #returns data in form of a json
+        response = requests.get(self.url, headers=self.headers)
+        data = response.json()
+        return data
+
+    
+if __name__ == "__main__":
+    airtable = Airtable()
+    print(airtable.getData())
