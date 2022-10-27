0656509a410643c8363f2251bb1d88851890b99f
Aznpepsi <Jamesbanh@yahoo.com>
2022-10-27 01:09:58 -0700

    changes

--- src/Airtable.py
+++ src/Airtable.py
@@ -3,11 +3,11 @@ import json
 
 class Airtable:
     def __init__(self):
-        api_key = "put api key here"
+        api_key = "key46Xfxr0liGO20W"
         self.headers = {"Authorization": "Bearer " + api_key}
         self.table_names = ["body-part", "block", "surgery", "references", "surgeon-preference", "keywords", "surgeon"]
-        self.base_id = "shrPk12YITH9VeP7j"
-        self.url = "https://api.airtable.com/v0/" + self.base_id
+        self.base_id = "appskv3EhFA9Bt8T7"
+        self.url = "https://api.airtable.com/v0/" + self.base_id + "/" + self.table_names[1]
 
     def getData(self): #returns data in form of a json
         response = requests.get(self.url, headers=self.headers)
