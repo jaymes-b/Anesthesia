9079065b5835565ac9c5c862a2ed15d7322fe173
Aznpepsi <Jamesbanh@yahoo.com>
2022-11-06 14:19:43 -0800

    cors enabled

--- src/routes.py
+++ src/routes.py
@@ -1,16 +1,18 @@
 from flask import Flask, request
+from flask_cors import CORS
 from Airtable import *
 import json
 
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 airtable = Airtable()
 
 @app.route("/")
 def handle_start():
     return "hello"
 
-@app.route("/home")
+@app.route("/api/home")
 def handle_home():
     surgery_names = {}
     surgeries_list = []
@@ -24,7 +26,7 @@ def handle_home():
     return surgery_names
 
 
-@app.route('/search') # returns both names of blocks and surgeries relevant to query
+@app.route('/api/search') # returns both names of blocks and surgeries relevant to query
 def handle_search():
     output = {}
     query = request.args.get("query").lower() #query = knee
@@ -32,12 +34,12 @@ def handle_search():
     output["blocks_data"] = airtable.getBlocksbyQuery(query)
     return output
 
-@app.route('/surgery') #handles getting surgery details
+@app.route('/api/surgery') #handles getting surgery details
 def handle_surgery():
     surgery_name = request.args.get("surgeryName").lower() #SurgeryName = knee ACL repair
     return airtable.getSurgeryByKey(surgery_name)
 
-@app.route('/block') #handles bodypart --> block, only returns blocks
+@app.route('/api/block') #handles bodypart --> block, only returns blocks
 def handle_block():
     body_part = request.args.get("bodyPart").lower() #query = knee
     return airtable.getBlocksbyBodyPart(body_part)
